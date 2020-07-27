1c8b4cf96347ba7614a0ffe072fe1fbd8b765538
Carlos Fdez. Llamas <hello@sirikon.me>
2020-07-27 18:16:48 +0200

    Change variable names

--- src/services/job_execution_service.cr
+++ src/services/job_execution_service.cr
@@ -9,23 +9,23 @@ module Workr::Services::JobExecutionService
     job_execution_id = JobDataService.create_execution(job_info.name)
     puts "Running job #{job_info.name}##{job_execution_id}"
 
-    outputReader, outputWriter = IO.pipe(write_blocking: true)
+    output_reader, output_writer = IO.pipe(write_blocking: true)
 
     process = Process.new(
       command: job_info.entrypoint,
-      output: outputWriter,
-      error: outputWriter,
+      output: output_writer,
+      error: output_writer,
       chdir: job_info.path)
 
-    processFinished = Channel(Nil).new
-    outputFinished = Channel(Nil).new
+    process_finished = Channel(Nil).new
+    output_finished = Channel(Nil).new
 
     spawn do
       JobDataService.open_execution_output job_info.@name, job_execution_id do |file|
         writing = true
 
         spawn do
-          outputReader.each_byte do |byte|
+          output_reader.each_byte do |byte|
             bytes = Slice.new(1, byte)
             file.write(bytes)
             print String.new(bytes)
@@ -39,14 +39,14 @@ module Workr::Services::JobExecutionService
           end
         end
 
-        processFinished.receive
+        process_finished.receive
         writing = false
       end
-      outputFinished.send(nil)
+      output_finished.send(nil)
     end
 
     process.wait
-    processFinished.send(nil)
-    outputFinished.receive
+    process_finished.send(nil)
+    output_finished.receive
   end
 end
