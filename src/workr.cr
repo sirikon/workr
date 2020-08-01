56b220f4f9ca3f39091e3a1dedc5607ed83a1713
Carlos Fdez. Llamas <hello@sirikon.me>
2020-08-01 16:38:29 +0200

    Include version in cli options

--- src/workr.cr
+++ src/workr.cr
@@ -2,7 +2,7 @@ require "./configuration/config"
 require "./web/server"
 
 module Workr
-
+  VERSION = {{ `shards version`.stringify }}
   command = ARGV.size >= 1 ? ARGV[0] : "help"
 
   case command
@@ -12,12 +12,14 @@ module Workr
     puts "Commands:"
     puts "  configure - Configuration wizard"
     puts "  daemon    - Starts Workr web server"
+    puts "  version   - Displays Workr version"
   when "configure"
     Configuration.wizard
   when "daemon"
     Web::Server.run
+  when "version"
+    puts VERSION
   else
     print "Unknown command '#{command}'\n"
   end
-
 end
