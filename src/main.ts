0cece9041e89c0bc5656aae7e2964348c1392118
Carlos Fdez. Llamas <hello@sirikon.me>
2022-04-04 13:39:25 +0200

    wip

---
+++ src/main.ts
@@ -0,0 +1,54 @@
+const resp = await fetch("http://127.0.0.1:4646/v1/jobs", {
+  method: "POST",
+  body: JSON.stringify({
+    Job: {
+      ID: "example",
+      Datacenters: ["dc1"],
+      Type: "batch",
+      Meta: {
+        execution: "5",
+      },
+      TaskGroups: [
+        {
+          Name: "example-g",
+          Restart: {
+            Attempts: 0,
+          },
+          Tasks: [
+            {
+              Name: "example-t2",
+              Driver: "docker",
+              Config: {
+                image: "ubuntu:focal",
+                command: "bash",
+                args: [
+                  "-c",
+                  'for i in {1..100000}\ndo\necho "Log line number #$i"\nsleep 1\ndone',
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }),
+  headers: {
+    "Content-Type": "application/json",
+    "Accepts": "application/json",
+  },
+});
+if (resp.status >= 400) {
+  console.log(await resp.text());
+}
+const result = await resp.json();
+const resp2 = await fetch(
+  "http://127.0.0.1:4646/v1/evaluation/" + result.EvalID + "/allocations",
+);
+if (resp2.status >= 400) {
+  console.log(await resp.text());
+}
+
+const result2 = await resp2.json();
+console.log(
+  `http://localhost:4646/ui/allocations/${result2[0].ID}/example-t2`,
+);
